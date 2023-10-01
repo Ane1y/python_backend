129ce2ceb7eb707bc3729d6d4ac2cee49290144a
Anelya Dusaeva <an.dusaeva@gmail.com>
2023-10-01 21:14:08 +0300

    have some issues with integration tests

--- app/tests/test_integration.py
+++ app/tests/test_integration.py
@@ -5,7 +5,7 @@ from app.routers.questions import router as question_router
 
 app = FastAPI()
 app.include_router(question_router)
-host = "localhost"
+host = "0.0.0.0"
 port = 8000
 
 uvicorn.run(app, host=host, port=port)
